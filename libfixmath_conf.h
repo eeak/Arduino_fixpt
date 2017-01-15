8e8a15b44a7e69265bb696f22c99fe64e80c3a81
Ben Hencke <brainstar@gmail.com>
2017-01-15 08:48:11 -0800

    conf appropriate for esp8266

--- libfixmath_conf.h
+++ libfixmath_conf.h
@@ -1,15 +1,14 @@
 #ifndef _LIBFIXMATH_CONF_H
 #define _LIBFIXMATH_CONF_H
 
-#ifdef ARDUINO_ARCH_AVR
-// ARDUINO_ARCH_AVR might not be the optimum define to check on,
-// but nothing else is available...
-// AVR Architecture is 8bit & limited in memory
-#define FIXMATH_NO_64BIT
-#define FIXMATH_OPTIMIZE_8BIT
+
+// #define FIXMATH_NO_64BIT
+// #define FIXMATH_OPTIMIZE_8BIT
 #define FIXMATH_NO_CACHE
 //#define FIXMATH_NO_OVERFLOW
 //#define FIXMATH_NO_ROUNDING
-#endif
+#define FIXMATH_FAST_SIN
+//#define FIXMATH_SIN_LUT
+
 
 #endif
